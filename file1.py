ac724d34fd9cbfbe896f67c225ae71d73cb694d0
PALUPI <123082587+mgtsrpalupi@users.noreply.github.com>
2023-01-19 21:00:40 +0800

    Add files via upload
    belajar #1

---
+++ file1.py
@@ -0,0 +1,19 @@
+# print ("halo bapak")
+# print ("halo ibu")
+
+# a = "satu"
+# b = 'dua'
+# c = '3'
+# d = '4'
+
+# x = 2
+# y = 3
+# v = 1.
+
+# z = x+v
+# w = y-x
+# u = x-y
+
+# print (u)
+
+print ("hello word")
